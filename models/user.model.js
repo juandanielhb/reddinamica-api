0f3d9748b0aac18acf9e2e7a2ee1b8f09928bb19
Juan D. Hernandez <juandanielhb@gmail.com>
2019-03-14 18:33:43 -0500

    User model is created

--- models/user.model.js
+++ models/user.model.js
@@ -6,11 +6,17 @@ var schema = mongoose.Schema;
 var userSchema = schema({
     name:String,
     surname:String,
-    nick:String,
-    email:String,
     password:String,
+    email:String,
+    about:String,
+    state:String,
     role:String,
-    image:String
+    postgraduate:String,
+    image:String,
+    profession: { type: schema.ObjectId, ref: 'profession'},
+    institution: { type: schema.ObjectId, ref: 'institution'},
+    city: { type: schema.ObjectId, ref: 'city'},
+    created_at:String
 });
 
 module.exports = mongoose.model('user', userSchema);
