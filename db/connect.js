ff949f99144b3e4b6e1186f0fdbf622eb5e633fa
juandanielhb <juandanielhb@gmail.com>
2019-05-13 11:18:14 -0500

    Removing data from config.json and put it on .env and adding v1 of docker-compose.yml

--- db/connect.js
+++ db/connect.js
@@ -1,34 +1,42 @@
 const mongoose = require('mongoose');
 const { MONGO_HOST, MONGO_PORT, MONGO_DB } = process.env;
-const connectionUrl = `mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}`; 
+const connectionUrl = `mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}`;
 
-module.exports = (()=>{
+const options = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    reconnectTries: Number.MAX_VALUE,
+    reconnectInterval: 500,
+    connectTimeoutMS: 10000,
+};
+
+module.exports = (() => {
     let instance = null,
         isDisconnecting = false;
 
-        
+
     function connect() {
         console.log("Starting mongodb connection.");
-        return new Promise((resolve, reject)=>{
-            mongoose.connect(connectionUrl, {useNewUrlParser: true}, (err) => {
-                if(err) { reject(err); return; }
-                console.log(`Mongo connection is up!`);
+        return new Promise((resolve, reject) => {
+            mongoose.connect(connectionUrl, options, (err) => {
+                if (err) { reject(err); return; }
+                console.log(`MongoDB is connected`);
                 instance = mongoose;
                 resolve(instance);
             })
         });
 
     }
-    
 
-    function disconnect(){
-        if (instance && !isDisconnecting){
-            
+
+    function disconnect() {
+        if (instance && !isDisconnecting) {
+
             isDisconnecting = true;
             console.log("Disconnecting mongo instance");
-            return new Promise((resolve, reject)=>{
-                mongoose.disconnect((err)=>{
-                    if (err) { reject(err); isDisconnecting=false; return; }
+            return new Promise((resolve, reject) => {
+                mongoose.disconnect((err) => {
+                    if (err) { reject(err); isDisconnecting = false; return; }
                     console.log("Mongo instance was disconnected");
                     resolve();
                 });
